f1e174cedc32bab79ac04026b78ce95d6fa4ce3b
Hunor Andras <handras@halcyonmobile.com>
2018-08-28 13:24:48 +0300

    Added Food model and rest_framework.

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Food(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    date_modified = models.DateTimeField('date modified')
+    is_on_stock = models.BooleanField()
+    def __str__(self):
+        return self.name
