fa425d7c869516e54dbe3b98668e1af260761f2e
Hunor Andras <handras@halcyonmobile.com>
2018-08-29 11:30:26 +0300

    Added CRUD methods for Food model.

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import ListCreateFoodView
+from .views import ListCreateFoodView, FoodDetailView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('foods/', ListCreateFoodView.as_view(), name="foods-all")
+    path('foods/', ListCreateFoodView.as_view(), name="foods-all"),
+    path('foods/id=<int:pk>/', FoodDetailView.as_view(), name="food-detail")
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -12,7 +12,7 @@ def index(request):
     
 class ListCreateFoodView(generics.ListCreateAPIView):
     """
-    GET food/
+    GET foods/
     POST food/
     """
     queryset = Food.objects.all()
@@ -24,3 +24,51 @@ class ListCreateFoodView(generics.ListCreateAPIView):
         new_food = Food.objects.create(user=user, name=request.data["name"])
         return Response(data=FoodSerializer(new_food).data, status=status.HTTP_201_CREATED)
     
+class FoodDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET food/:id/
+    PUT food/:id/
+    DELETE food/:id/
+    """
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            a_food = self.queryset.get(pk=kwargs["pk"])
+            return Response(FoodSerializer(a_food).data)
+        except Food.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Food with id: {} does not exist".format(kwargs["pk"])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+#     @validate_request_data
+    def put(self, request, *args, **kwargs):
+        try:
+            a_food = self.queryset.get(pk=kwargs["pk"])
+            serializer = FoodSerializer()
+            updated_food = serializer.update(a_food, request.data)
+            return Response(FoodSerializer(updated_food).data)
+        except Food.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Food with id: {} does not exist".format(kwargs["pk"])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            a_food = self.queryset.get(pk=kwargs["pk"])
+            a_food.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Food.DoesNotExist:
+            return Response(
+                data={
+                    "message": "Food with id: {} does not exist".format(kwargs["pk"])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
